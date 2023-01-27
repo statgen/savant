ae85f88618e3da36c2333568506b03396a64ca55
jonathonl <jonathonl@users.noreply.github.com>
2023-01-26 19:14:06 -0500

    Adds inverse_normalize function.

---
+++ include/inv_norm.hpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <functional>
+#include <cstdint>
+#include <algorithm>
+
+/*
+ * Adapted from https://github.com/hyunminkang/invNorm
+ */
+
+double stdnormal_cdf(double u);
+double stdnormal_inv(double p);
+
+template <typename T>
+void inverse_normalize(std::vector<T>& vec, std::vector<std::reference_wrapper<T>>& rank_vec)
+{
+  std::size_t n = vec.size();
+  rank_vec.assign(vec.begin(), vec.end());
+
+  std::sort(rank_vec.begin(), rank_vec.end(), [](const T& l, const T& r) { return l < r; });
+
+  std::size_t left = 0;
+  T prev_val = rank_vec[0];
+  for(std::size_t i=1; i < rank_vec.size(); ++i)
+  {
+    if ( prev_val == rank_vec[i] )
+    {
+      // do not resolve ties yet
+    }
+    else
+    {
+      // tie can be resolved from left ... i-1
+      T v(stdnormal_inv((left + i)/2./n));
+      for (std::size_t j = left; j < i; ++j)
+        rank_vec[j].get() = v;
+      prev_val = rank_vec[i];
+      left = i;
+    }
+  }
+}
+
+template <typename T>
+void inverse_normalize(std::vector<T>& vec)
+{
+  std::vector<std::reference_wrapper<T>> tmp;
+  inverse_normalize(vec, tmp);
+}
