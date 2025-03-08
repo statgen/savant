b3e0e76c2a768c53ac8b91a6b2be65e10c17388f
jonathonl <jonathonl@users.noreply.github.com>
2025-03-08 14:47:39 -0500

    Checkpoints QTL burden.

--- src/qtl.cpp
+++ src/qtl.cpp
@@ -15,6 +15,10 @@
 
 #include <cstdlib>
 #include <random>
+#include <list>
+#include <unordered_set>
+#include <string>
+#include <utility>
 
 class qtl_prog_args : public getopt_wrapper
 {
@@ -22,6 +26,9 @@ private:
   //std::string sub_command_;
   //std::vector<option> long_options_;
   //std::string short_options_;
+  std::unordered_set<std::string> consequences_;
+  std::unordered_set<std::string> impacts_;
+  std::list<std::pair<std::string, savvy::genomic_region>> collapse_regions_;
   std::string cov_path_;
   //std::string phenotype_field_;
   std::string geno_path_;
@@ -32,6 +39,7 @@ private:
   std::string debug_log_path_ = "/dev/null";
   std::string fmt_field_ = "";
   std::unique_ptr<savvy::genomic_region> region_;
+  double rare_treshold_ = 0.01;
   double min_mac_ = 1.0;
   double min_maf_ = 0.f;
   double max_pval_ = 2.;
@@ -45,6 +53,10 @@ private:
 public:
   qtl_prog_args() :
     getopt_wrapper("Usage: savant qtl [opts ...] <geno_file> <pheno_file>", {
+      {"collapse-regions", "<file>", 'R', "BED file of regions to collapse for rare variant testing"},
+      {"collapse-threshold", "<real>", 'T', "AF threshold for burden test (default: 0.01)"},
+      {"collapse-anno", "<string>", 'A', "Comma-separated list of INFO/ANN annotation values to include in analysis (see https://pcingola.github.io/SnpEff/adds/VCFannotationformat_v1.0.pdf)"},
+      {"collapse-impact", "<string>", 'I', "Comma-separated list of INFO/ANN impact values to include in analysis (see https://pcingola.github.io/SnpEff/adds/VCFannotationformat_v1.0.pdf)"},
       {"cov", "<file>", 'c', "Covariates file"},
       {"debug-log", "<file>", '\x02', "Enables debug logging and specifies log file"},
       {"discovery-counts", "<file>", '\x02', "Writes discovery counts to specified file for use in empirical FDR calculation"},
@@ -81,6 +93,9 @@ public:
 
   virtual ~qtl_prog_args() {}
 
+  const std::unordered_set<std::string>& consequences() const { return consequences_; }
+  const std::unordered_set<std::string>& impacts() const { return impacts_; }
+  const std::list<std::pair<std::string, savvy::genomic_region>>& collapse_regions() const { return collapse_regions_; }
   const std::string& cov_path() const { return cov_path_; }
   const std::string& geno_path() const { return geno_path_; }
   const std::string& pheno_path() const { return pheno_path_; }
@@ -90,6 +105,7 @@ public:
   const std::string& fmt_field() const { return fmt_field_; }
   const std::string& debug_log_path() const { return debug_log_path_; }
   const std::unique_ptr<savvy::genomic_region>& region() const { return region_; }
+  double rare_threshold() const { return rare_treshold_; }
   double min_mac() const { return min_mac_; }
   double min_maf() const { return min_maf_; }
   double max_pval() const { return max_pval_; }
@@ -126,6 +142,29 @@ public:
       if (fmt_avail.find(fmt_field_) == fmt_avail.end())
         return std::cerr << "Error: requested format field (" << fmt_field_ << ") not found in file headers\n", false;
     }
+    return true;
+  }
+
+  static bool parse_bed_file(const std::string& filepoath, std::list<std::pair<std::string, savvy::genomic_region>>& dest)
+  {
+
+    std::string line;
+    std::ifstream ifs(filepoath);
+    if (!ifs)
+      return std::cerr << "Error: invalid file path\n", false;
+
+    if (!std::getline(ifs, line))
+      return std::cerr << "Error: BED file empty\n", false;
+
+    while (std::getline(ifs, line))
+    {
+      auto fields = utility::split_string_to_vector(line, '\t');
+      if (fields.size() < 4)
+        return std::cerr << "Error: BED file must have at least 4 columns\n", false;
+
+      dest.emplace_back(fields[3], savvy::genomic_region(fields[0], std::atoll(fields[1].c_str()) + 1, std::atoll(fields[2].c_str())));
+    }
+
     return true;
   }
 
@@ -210,6 +249,10 @@ public:
         break;
       case 'r':
         region_.reset(new savvy::genomic_region(utility::string_to_region(optarg ? optarg : "")));
+        break;
+      case 'R':
+        if (!parse_bed_file(optarg ? optarg : "", collapse_regions_))
+          return std::cerr << "Error: failed to parse --collapse-regions file\n", false;
         break;
       default:
         return false;
@@ -593,6 +636,70 @@ public:
   }
 };
 
+class collapse_output_wrapper
+{
+private:
+  std::vector<std::unique_ptr<shrinkwrap::bgzf::ostream>> out_files;
+  std::vector<std::string> pheno_names_;
+  bool split_;
+public:
+  collapse_output_wrapper(const std::string& output_path, std::vector<std::string> pheno_names, bool split_output) :
+    pheno_names_(std::move(pheno_names)),
+    split_(split_output)
+  {
+    if (split_)
+    {
+      out_files.resize(pheno_names_.size());
+      for (std::size_t i = 0; i < out_files.size(); ++i)
+        out_files[i] = std::make_unique<shrinkwrap::bgzf::ostream>(output_path + pheno_names_[i] + ".tsv.gz");
+    }
+    else
+    {
+      out_files.emplace_back(std::make_unique<shrinkwrap::bgzf::ostream>(output_path));
+    }
+
+    for (std::size_t i = 0; i < out_files.size(); ++i)
+    {
+      *out_files[i] << "chrom\tbeg\tend\tregion_id\trare_threshold\tn_carriers\tns\t" << linear_model::stats_t::header_column_names();
+      if(!split_)
+        *out_files[i] << "\tpheno_id";
+      *out_files[i] << std::endl;
+    }
+  }
+
+  bool write(const savvy::genomic_region& reg, const std::string& reg_id, float rare_threshold, std::int64_t n_carriers, std::int64_t ns, const linear_model::stats_t& stats, std::size_t pheno_idx)
+  {
+    std::size_t file_idx = 0;
+    if (split_)
+      file_idx = pheno_idx;
+    (*out_files[file_idx]) << reg.chromosome()
+                           << "\t" << reg.from()
+                           << "\t" << reg.to()
+                           << "\t" << reg_id
+                           << "\t" << rare_threshold
+                           << "\t" << n_carriers
+                           << "\t" << ns
+                           << "\t" << stats;
+    if (!split_)
+      (*out_files[file_idx]) << "\t" << pheno_names_[pheno_idx];
+    (*out_files[file_idx]) << "\n";
+
+    return out_files[file_idx]->good();
+  }
+
+  bool close()
+  {
+    for (std::size_t i = 0; i < out_files.size(); ++i)
+    {
+      out_files[i]->flush();
+      if (!out_files[i]->good())
+        return std::cerr << "Error: output file close failure\n", false;
+    }
+
+    return true;
+  }
+};
+
 class discovery_counter
 {
 private:
@@ -677,6 +784,76 @@ public:
     return ofs.good();
   }
 };
+
+bool process_burden_vector(const savvy::genomic_region& reg, const std::string& reg_id, const std::vector<std::vector<scalar_type>>& pheno_resids, const std::vector<linear_model::variable_stats<scalar_type>>& pheno_stats,  const std::vector<residualizer>& residualizers, const std::vector<std::vector<std::size_t>>& subset_non_missing_map, std::vector<scalar_type>& burden, collapse_output_wrapper& output_file, const qtl_prog_args& args, std::size_t ploidy, std::vector<discovery_counter>& discovery_counts)
+{
+  std::size_t n_perm = 0;
+  if (!discovery_counts.empty())
+    n_perm = discovery_counts.size() - 1;
+  std::size_t stride = n_perm + 1;
+
+  std::vector<scalar_type> burden_sub;
+
+  for (std::size_t pheno_idx = 0; pheno_idx < pheno_resids.size(); ++pheno_idx)
+  {
+    const std::vector<scalar_type>& pheno_ref = pheno_resids[pheno_idx];
+    const std::vector<std::size_t>& pheno_map_ref = subset_non_missing_map[pheno_idx];
+
+    std::size_t n = pheno_resids[pheno_idx].size() / stride;
+
+    //double ac = geno_stats_sub.sum();
+    std::size_t an = n * ploidy;
+    //double af = ac / an;
+    //double mac = (ac > (an/2.f) ? an - ac : ac);
+    //double maf = (af > 0.5f ? 1.f - af : af);
+
+    if (an == 0) continue;
+//    if (mac < args.min_mac()) continue;
+//    if (maf < args.min_maf()) continue;
+
+    burden_sub.resize(n);
+
+    for (std::size_t i = 0; i < burden.size(); ++i)
+    {
+      std::size_t off = pheno_map_ref[i];
+      if (off < n)
+      {
+        burden_sub[off] = burden[i];
+      }
+    }
+
+    std::int64_t n_carriers = n - std::count(burden_sub.begin(), burden_sub.end(), scalar_type());
+
+    burden_sub = residualizers[residualizers.size() == 1 ? 0 : pheno_idx](burden_sub, false);
+
+    scalar_type s_x = 0;
+    scalar_type s_xx = 0;
+    std::vector<scalar_type> s_xy(stride);
+    for (std::size_t i = 0; i < burden_sub.size(); ++i)
+    {
+      s_x += burden_sub[i];
+      s_xx += burden_sub[i] * burden_sub[i];
+      for (std::size_t j = 0; j < stride; ++j)
+        s_xy[j] += burden_sub[i] * pheno_ref[i * stride + j];
+    }
+
+
+    std::vector<linear_model::stats_t> stats_dense(stride);
+    for (std::size_t i = 0; i < stride; ++i)
+      stats_dense[i] = linear_model::ols(n, s_xy[i], linear_model::variable_stats<scalar_type>(burden_sub), pheno_stats[pheno_idx], burden_sub.size() - (residualizers[residualizers.size() == 1 ? 0 : pheno_idx].n_predictors()) - 2);
+
+    if (stride > 1)
+    {
+      for (std::size_t i = 0; i < stride; ++i)
+        discovery_counts[i](args.rare_threshold(), stats_dense[i]);
+    }
+
+    if (stats_dense[0].pvalue > args.max_pval()) continue;
+    if (!output_file.write(reg, reg_id, args.rare_threshold(), n_carriers, n, stats_dense[0], pheno_idx))
+      return std::cerr << "Error: failed writing to output file\n", false;
+  }
+  return true;
+}
 
 template <typename GenoT>
 bool process_variant(const savvy::site_info& var, const std::vector<std::vector<scalar_type>>& pheno_resids, const std::vector<linear_model::variable_stats<scalar_type>>& pheno_stats,  const std::vector<residualizer>& residualizers, const std::vector<std::vector<std::size_t>>& subset_non_missing_map, const savvy::compressed_vector<GenoT>& geno, std::vector<scalar_type>& geno_dense, const linear_model::variable_stats<scalar_type>& geno_stats, output_wrapper& output_file, const qtl_prog_args& args, std::size_t ploidy, std::vector<discovery_counter>& discovery_counts)
@@ -794,6 +971,122 @@ bool process_variant(const savvy::site_info& var, const std::vector<std::vector<
   return true;
 }
 
+bool process_collapse(const std::vector<std::vector<scalar_type>>& pheno_resids, const std::vector<linear_model::variable_stats<scalar_type>>& pheno_stats, std::vector<discovery_counter>& discovery_counts, const std::vector<residualizer>& residualizers, const std::vector<std::vector<std::size_t>>& subset_non_missing_map, savvy::reader& geno_file, collapse_output_wrapper& output_file, const qtl_prog_args& args)
+{
+  if (pheno_resids.empty())
+    return false;
+
+  std::unordered_set<std::string> impacts = {"HIGH"}; // TODO:
+  savvy::genomic_region reg = args.collapse_regions().front().second; // TODO:
+  std::string reg_id = args.collapse_regions().front().first;
+
+  if (!geno_file.reset_bounds(reg))
+    return std::cerr << "Could not open genomic region\n", false;
+
+  auto total_start = std::chrono::high_resolution_clock::now();
+
+  savvy::compressed_vector<std::int8_t> geno;
+  std::vector<scalar_type> burden_dense;
+  savvy::variant var;
+  std::string ann;
+  std::vector<std::string> ann_vec;
+  while (geno_file.read(var))
+  {
+    double af = std::numeric_limits<double>::quiet_NaN();
+    std::int64_t ac, an;
+    if (!var.get_info("AF", af))
+    {
+      if (var.get_info("AC", ac) && var.get_info("AN", an))
+        af = double(ac) / double(an);
+    }
+
+    if (std::isnan(af))
+      return std::cerr << "Error: AF or AC/AN INFO fields must be present for burden testing\n", false;
+
+    if (af >= args.rare_threshold()) continue;
+
+    bool geno_ready = false;
+
+    bool fetch_ann = true;
+    if (fetch_ann)
+    {
+      if (!var.get_info("ANN", ann))
+        return std::cerr << "Error: INFO/ANN must be present when using --collapse-anno or --collapse-impact\n", false;
+      ann_vec = utility::split_string_to_vector(ann, ',');
+    }
+
+    for (std::size_t alt_idx = 1; alt_idx <= var.alts().size(); ++alt_idx)
+    {
+      if (fetch_ann)
+      {
+        std::string allele = var.alts()[alt_idx-1];
+        bool process_allele = false;
+        for (auto it = ann_vec.begin(); it != ann_vec.end() && !process_allele; ++it)
+        {
+          auto ann_fields = utility::split_string_to_vector(*it, '|');
+          if (ann_fields.size() > 2 && ann_fields[0] == allele)
+          {
+            if (impacts.size() && impacts.find(ann_fields[2]) != impacts.end())
+              process_allele = true;
+          }
+        }
+
+        if (process_allele)
+        {
+          if (!geno_ready)
+          {
+            var.get_format("GT", geno);
+            if (burden_dense.empty())
+              burden_dense.resize(geno.size());
+            else if (burden_dense.size() != geno.size())
+              return std::cerr << "Error: inconsistent ploidy in FORMAT field\n", false;
+            geno_ready = true;
+          }
+
+          for (auto gt = geno.begin(); gt != geno.end(); ++gt)
+          {
+            if (*gt == alt_idx)
+              burden_dense[gt.offset()] += 1; // TODO: maybe add weight by AF option
+            else if (savvy::typed_value::is_end_of_vector(*gt))
+              burden_dense[gt.offset()] = *gt;
+          }
+        }
+      }
+    }
+  }
+
+  assert(!subset_non_missing_map.empty());
+  std::size_t ploidy = burden_dense.size() / subset_non_missing_map[0].size();
+  savvy::stride_reduce(burden_dense, ploidy, savvy::plus_eov<scalar_type>());
+
+  if (!process_burden_vector(reg, reg_id, pheno_resids, pheno_stats, residualizers, subset_non_missing_map, burden_dense, output_file, args, ploidy, discovery_counts))
+    return false;
+
+  std::cerr << "total_duration:" << std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - total_start).count() << std::endl;
+
+  if (geno_file.bad())
+    return std::cerr << "Error: input file read error\n", false;
+
+  //  std::cerr << "subset_duration:" << std::chrono::duration_cast<std::chrono::seconds>(subset_duration).count() << std::endl;
+  //  std::cerr << "stride_reduce_duration:" << std::chrono::duration_cast<std::chrono::seconds>(stride_reduce_duration).count() << std::endl;
+  //  std::cerr << "stats_duration:" << std::chrono::duration_cast<std::chrono::seconds>(stats_duration).count() << std::endl;
+  //  std::cerr << "ols_duration:" << std::chrono::duration_cast<std::chrono::seconds>(ols_duration).count() << std::endl;
+  //  std::cerr << "ols_dense_duration:" << std::chrono::duration_cast<std::chrono::seconds>(ols_dense_duration).count() << std::endl;
+  //  std::cerr << "write_duration (ms):" << std::chrono::duration_cast<std::chrono::microseconds>(write_duration).count() << std::endl;
+
+  if (!discovery_counts.empty())
+  {
+    std::ofstream discovery_file(args.discovery_counts_path());
+    if (!discovery_counter::write(discovery_counts, discovery_file))
+      return std::cerr << "Error: failed writing discovery counts file\n", false;
+    discovery_file.close();
+    if (!discovery_file.good())
+      return std::cerr << "Error: failed closing discovery counts file\n", false;
+  }
+
+  return true;
+}
+
 bool process_trans_batch(const std::vector<std::vector<scalar_type>>& pheno_resids, const std::vector<linear_model::variable_stats<scalar_type>>& pheno_stats, std::vector<discovery_counter>& discovery_counts, const std::vector<residualizer>& residualizers, const std::vector<std::vector<std::size_t>>& subset_non_missing_map, savvy::reader& geno_file, /*std::ostream*/output_wrapper& output_file, const qtl_prog_args& args)
 {
   if (pheno_resids.empty())
@@ -805,10 +1098,10 @@ bool process_trans_batch(const std::vector<std::vector<scalar_type>>& pheno_resi
   auto total_start = std::chrono::high_resolution_clock::now();
 
   savvy::compressed_vector<std::int8_t> geno;
-  savvy::compressed_vector<std::int8_t> geno_sub;
+  //savvy::compressed_vector<std::int8_t> geno_sub;
   savvy::compressed_vector<std::int8_t> geno_biallele;
   std::vector<scalar_type> geno_dense;
-  savvy::variant var; std::size_t progress = 0;
+  savvy::variant var;
   while (geno_file.read(var))
   {
     var.get_format("GT", geno);
@@ -1068,13 +1361,24 @@ int trans_qtl_main(int argc, char** argv)
     }
   }
 
-  //shrinkwrap::bgzf::ostream output_file(args.output_path());
-  //output_file << "geno_chrom\tgeno_pos\tref\talt\tvariant_id\tmaf\tmac\tns\t" << linear_model::stats_t::header_column_names() << "\tpheno_id" << std::endl;
-  output_wrapper output(args.output_path(), pheno_names, args.split_output());
+  if (args.collapse_regions().size())
+  {
+    collapse_output_wrapper output(args.output_path(), pheno_names, args.split_output());
 
-  if (!process_trans_batch(pheno_resids, pheno_stats, discovery_counts, residualizers, subset_non_missing_map, geno_file, output, args))
-    return std::cerr << "Error: processing batch failed\n", EXIT_FAILURE;
+    if (!process_collapse(pheno_resids, pheno_stats, discovery_counts, residualizers, subset_non_missing_map, geno_file, output, args))
+      return std::cerr << "Error: processing batch failed\n", EXIT_FAILURE;
 
+    return output.close() ? EXIT_SUCCESS : EXIT_FAILURE;
+  }
+  else
+  {
+    // shrinkwrap::bgzf::ostream output_file(args.output_path());
+    // output_file << "geno_chrom\tgeno_pos\tref\talt\tvariant_id\tmaf\tmac\tns\t" << linear_model::stats_t::header_column_names() << "\tpheno_id" << std::endl;
+    output_wrapper output(args.output_path(), pheno_names, args.split_output());
 
-  return output.close() ?  EXIT_SUCCESS : EXIT_FAILURE;
+    if (!process_trans_batch(pheno_resids, pheno_stats, discovery_counts, residualizers, subset_non_missing_map, geno_file, output, args))
+      return std::cerr << "Error: processing batch failed\n", EXIT_FAILURE;
+
+    return output.close() ? EXIT_SUCCESS : EXIT_FAILURE;
+  }
 }
